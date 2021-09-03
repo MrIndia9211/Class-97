24fd67792000851afec267c3e30866985226dcb4
Raghavmalik123 <71924202+Raghavmalik123@users.noreply.github.com>
2021-09-03 03:17:44 -0700

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,12 @@
+# age=int(input("Enter Your Age : "));
+# if(age>18):
+#     print("You Are An Adult You Can Vote")
+# elif(age>12):
+#     print("You Are A Teen Ager You Can Not Vote")
+# else:
+#     print("You Are Still A Kid")
+    
+count=5
+while(count>=0):
+    print(count)
+    count=count-1
